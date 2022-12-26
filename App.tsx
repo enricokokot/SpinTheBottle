5ac8af3c056e20450d9fa603303c6654a3551d84
coco <kokotenrico@gmail.com>
2022-12-26 12:16:19 +0100

    Clean up top component

--- App.tsx
+++ App.tsx
@@ -1,36 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren, useState, useEffect} from 'react';
-import {
-  Animated,
-  Pressable,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  Touchable,
-  TouchableOpacity,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import Die from './Die';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import HotDice from './HotDice';
@@ -42,8 +10,8 @@ const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator screenOptions={{tabBarStyle: {display: 'none'}}}>
-        <Tab.Screen name="HotDice" component={HotDice} />
         <Tab.Screen name="LiarsDice" component={LiarsDice} />
+        <Tab.Screen name="HotDice" component={HotDice} />
       </Tab.Navigator>
     </NavigationContainer>
   );
